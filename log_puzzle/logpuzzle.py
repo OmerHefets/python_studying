c7f743e909c77e322d548127b15390fc72d424e7
Omer Hefets <hefets.omer@gmail.com>
2019-06-07 19:17:29 +0300

    added read_file and read_urls functions

--- log_puzzle/logpuzzle.py
+++ log_puzzle/logpuzzle.py
@@ -3,14 +3,25 @@ import sys
 import os
 
 GVAHIM_URL = "http://data.cyber.org.il/python/logpuzzle/"
+MESSAGE_FILE = "log_files/message_data.cyber.org.il"
+LOGO_DATA_FILE = "log_files/logo_data.cyber.org.il"
+URL_STRING = "/python/logpuzzle/"
 
 
-def read_file():
-    pass
+def read_file(source_path):
+    with open(source_path, "r") as file:
+        line_list = file.readlines()
+        return line_list
 
 
-def read_urls():
-    pass
+def read_urls(list_of_lines):
+    list_of_urls = []
+    for line in list_of_lines:
+        if URL_STRING in line:
+            start_index = line.find(URL_STRING)
+            search_for_space = line[start_index:]
+            end_index = search_for_space.find(' ')
+            print(search_for_space[:end_index])
 
 
 def sort_urls():
@@ -26,6 +37,9 @@ def add_urls_to_html():
 
 
 def main():
+    urls = []
+    lines = read_file(LOGO_DATA_FILE)
+    read_urls(lines)
     pass
 
 
