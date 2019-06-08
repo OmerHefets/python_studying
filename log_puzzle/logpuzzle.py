aab1c46b4e0de7f0e52d0ece2f2a0b59db020493
Omer Hefets <hefets.omer@gmail.com>
2019-06-08 19:13:31 +0300

    completed downloading function

--- log_puzzle/logpuzzle.py
+++ log_puzzle/logpuzzle.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib.request
 import sys
 import os
 
@@ -6,6 +6,10 @@ GVAHIM_URL = "http://data.cyber.org.il"
 MESSAGE_FILE_PATH = "log_files/message_data.cyber.org.il"
 LOGO_DATA_FILE_PATH = "log_files/logo_data.cyber.org.il"
 URL_STRING = "/python/logpuzzle/"
+FOLDER_PATH = "~/programming/python/python_studying/log_puzzle"
+LOGO_FOLDER_PATH = "logo_images"
+MESSAGE_FOLDER_PATH = "message_images"
+
 FIRST = True
 SECOND = False
 
@@ -21,30 +25,26 @@ def read_urls(list_of_lines):
     for line in list_of_lines:
         if URL_STRING in line:
             start_index = line.find(URL_STRING)
-            search_for_space = line[start_index:]
-            end_index = search_for_space.find(' ')
-            list_of_urls.append(search_for_space[:end_index])
+            url_from_start_index = line[start_index:]
+            end_index = url_from_start_index.find(' ')
+            full_url = url_from_start_index[:end_index]
+            if full_url not in list_of_urls:
+                list_of_urls.append(full_url)
     return list_of_urls
 
 
-def sort_logo_urls(list_of_urls):
+def sort_urls(list_of_urls, message):
     list_of_words = []
-    for url in list_of_urls:
-        word = extract_word_from_url(url, first_or_second=FIRST)
-        list_of_words.append(word)
-    zipped_lists = zip(list_of_words, list_of_urls)
-    zipped_lists = sorted(zipped_lists)
-    sorted_urls_list = [x for y, x in zipped_lists]
-    return sorted_urls_list
-
-
-def sort_message_urls(list_of_urls):
-    list_of_words = []
-    for url in list_of_urls:
-        first_word = extract_word_from_url(url, first_or_second=FIRST)
-        second_word = extract_word_from_url(url, first_or_second=SECOND)
-        concat_words = second_word + first_word
-        list_of_words.append(concat_words)
+    if not message:
+        for url in list_of_urls:
+            word = extract_word_from_url(url, first_or_second=FIRST)
+            list_of_words.append(word)
+    else:
+        for url in list_of_urls:
+            first_word = extract_word_from_url(url, first_or_second=FIRST)
+            second_word = extract_word_from_url(url, first_or_second=SECOND)
+            concat_words = second_word + first_word
+            list_of_words.append(concat_words)
     zipped_lists = zip(list_of_words, list_of_urls)
     zipped_lists = sorted(zipped_lists)
     sorted_urls_list = [x for y, x in zipped_lists]
@@ -74,8 +74,10 @@ def nth_occurrence_index(string, char, n):
 
 def download_images(sorted_urls, dest_folder):
     for i in range(len(sorted_urls)):
-        concat_prefix = GVAHIM_URL + sorted_urls[i]
-        print(concat_prefix)
+        url_with_prefix = GVAHIM_URL + sorted_urls[i]
+        filename = 'img' + str(i)
+        filename_and_path = dest_folder + '/' + filename + '.jpg'
+        urllib.request.urlretrieve(url_with_prefix, filename_and_path)
 
 
 def write_html_gui():
@@ -86,7 +88,7 @@ def add_urls_to_html():
     pass
 
 
-def main():
+def main(download):
     # read urls:
     logo_lines = read_file(LOGO_DATA_FILE_PATH)
     logo_urls = read_urls(logo_lines)
@@ -94,13 +96,15 @@ def main():
     message_urls = read_urls(message_lines)
 
     # sort urls:
-    sorted_logo_urls = sort_logo_urls(logo_urls)
-    sorted_massage_urls = sort_message_urls(message_urls)
+    sorted_logo_urls = sort_urls(logo_urls, message=False)
+    sorted_message_urls = sort_urls(message_urls, message=True)
 
     # download the images:
-    download_images(sorted_massage_urls, True)
+    if download:
+        download_images(sorted_message_urls, MESSAGE_FOLDER_PATH)
+        download_images(sorted_logo_urls, LOGO_FOLDER_PATH)
     # write to the html file:
 
 
 if __name__ == "__main__":
-    main()
+    main(download=False)
