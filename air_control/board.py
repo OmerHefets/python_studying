1f31509fe38aa09942e7e18179d57fd649067e8a
Omer Hefets <hefets.omer@gmail.com>
2019-06-10 23:47:43 +0300

    finished initializing the board, basic constants file

--- air_control/board.py
+++ air_control/board.py
@@ -1,18 +1,20 @@
 import pygame
 import random
+from .constants import ###
 # from planes import Plane
-
 
 NUM_OF_IMAGES_PER_LINE = 10
 IMAGE_HEIGHT = 50  # pixels
 IMAGE_WIDTH = 50  # pixels
 LINE_HEIGHT_PIXEL = 2
-NUMBER_OF_LINES = NUM_OF_IMAGES_PER_LINE + 1
+NUM_OF_LINES = NUM_OF_IMAGES_PER_LINE + 1
+NUM_OF_PLANES = 4
 
-WINDOW_HEIGHT = (IMAGE_HEIGHT * NUM_OF_IMAGES_PER_LINE) + (LINE_HEIGHT_PIXEL * NUMBER_OF_LINES)
-WINDOW_WIDTH = (IMAGE_WIDTH * NUM_OF_IMAGES_PER_LINE) + (LINE_HEIGHT_PIXEL * NUMBER_OF_LINES)
+WINDOW_HEIGHT = (IMAGE_HEIGHT * NUM_OF_IMAGES_PER_LINE) + (LINE_HEIGHT_PIXEL * NUM_OF_LINES)
+WINDOW_WIDTH = (IMAGE_WIDTH * NUM_OF_IMAGES_PER_LINE) + (LINE_HEIGHT_PIXEL * NUM_OF_LINES)
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 
 def init_screen():
@@ -22,26 +24,37 @@ def init_screen():
     pygame.display.set_caption("Air Control")
     screen.fill(WHITE)
     pygame.display.flip()
-    for i in range(NUM_OF_IMAGES_PER_LINE + 1):
-        pass
+    # init horizontal lines
+    for i in range(NUM_OF_LINES):
+        current_x_location = i * IMAGE_WIDTH + i * LINE_HEIGHT_PIXEL
+        pygame.draw.line(screen, BLACK, [current_x_location, 0], [current_x_location, WINDOW_HEIGHT], LINE_HEIGHT_PIXEL)
+        pygame.display.flip()
+    # init vertical lines
+    for i in range(NUM_OF_LINES):
+        current_y_location = i * IMAGE_HEIGHT + i * LINE_HEIGHT_PIXEL
+        pygame.draw.line(screen, BLACK, [0, current_y_location], [WINDOW_WIDTH, current_y_location], LINE_HEIGHT_PIXEL)
+        pygame.display.flip()
+
+    init_game(number_of_planes=NUM_OF_PLANES)
+    pygame.quit()
+
+
+def init_game(number_of_planes):
     finish = False
     while not finish:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finish = True
-    pygame.quit()
+        init_planes(number_of_planes=NUM_OF_PLANES)
 
 
-def init_game():
-    pass
-
-
-def init_planes():
+def init_planes(number_of_planes):
     """
     Make sure that the planes do not start in the same position.
     :return:
     """
-    pass
+    planes_list = pygame.sprite.Group()
+
 
 
 def movement_options():
--- air_control/planes.py
+++ air_control/planes.py
@@ -1,18 +1,34 @@
+import random
+from . import constants as cst
+
+
 class Plane:
 
     def __init__(self, x, y):
-        pass
+        self.__x = x
+        self.__y = y
 
     def get_position(self):
-        pass
+        return self.__x, self.__y
 
-    def set_position(self, random):
+    def set_position(self, rand, bounds, x, y):
         """
         Set a starting position for a plane.
-        :param random: return random position
-        :return:
+        0 < X < IMG_PER_LINE - 1
+        0 < Y < IMG_PER_LINE - 1
+        :param rand: return random position
+        :param bounds: set the range of the random init
+        :param x: X set if not random
+        :param y: Y set if not random
+        :return: set position
         """
-        pass
+        if not rand:
+            self.__x = x
+            self.__y = y
+            return self.__x, self.__y
+        else:
+            self.__x = random.randint(0, cst.NUM_OF_IMAGES_PER_LINE)
+            ###
 
 
 def main():
